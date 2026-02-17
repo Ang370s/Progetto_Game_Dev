7d57b52dc849c9fe2b4adb4910f56567edb14341
malele <166812730+malele10@users.noreply.github.com>
2026-02-17 17:18:11 +0100

    Grafica Scene
    Sistemato tutte le grafiche delle scene
    Da fare la logica dei records
    
    Co-Authored-By: Ang370s <166812389+Ang370s@users.noreply.github.com>
    Co-Authored-By: hugh27 <203445311+hugh27@users.noreply.github.com>

--- Assets/Scripts/Game Manager/VictoryManager.cs
+++ Assets/Scripts/Game Manager/VictoryManager.cs
@@ -15,7 +15,7 @@ public class VictoryManager : MonoBehaviour
             int totale = PlayerStats.Instance.GetFinalScore();
 
             // 2. Scrive il testo a schermo
-            scoreDisplay.text = "PUNTEGGIO: " + totale.ToString();
+            scoreDisplay.text = totale.ToString();
 
             // 3. Nascondi la vecchia UI del dungeon (i contatori piccoli)
             Canvas oldUI = PlayerStats.Instance.GetComponentInChildren<Canvas>();
@@ -27,7 +27,7 @@ public class VictoryManager : MonoBehaviour
         else
         {
             // Se arrivi qui senza passare dal gioco (es. lanci la scena da sola)
-            scoreDisplay.text = "PUNTEGGIO: 0";
+            scoreDisplay.text = "0";
             Debug.LogWarning("PlayerStats non trovato. Hai lanciato la scena direttamente?");
         }
     }
